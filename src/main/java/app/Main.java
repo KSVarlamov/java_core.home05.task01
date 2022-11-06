a2db4b9d497c9146b74e8aef25d6c4ad2c6c48bd
varlamov.ks <varlamov.ks@mail.ru>
2022-11-06 13:14:04 +0300

    Решение задачи

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,2 +1,52 @@
-package app;public class Main {
+package app;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+
+        String json = listToJson(list);
+        try (FileWriter fileWriter = new FileWriter(new File("data.json"))) {
+            fileWriter.write(json);
+            fileWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(list, listType);
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(new File(fileName)))) {
+            ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csv.parse();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
